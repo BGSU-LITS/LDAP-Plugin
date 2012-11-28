a720b8aed4ac978102d30e3203c822bdb3052614
Dave Widmer <dwidmer@bgsu.edu>
2012-11-28 16:42:06 -0500

    Adding the filters back into the plugin.

--- LdapPlugin.php
+++ LdapPlugin.php
@@ -15,18 +15,15 @@ class LdapPlugin extends Omeka_Plugin_AbstractPlugin
 	/**
 	 * @var array  The filters used in this plugin.
 	 */
-	protected $_filters = array(
-		//'login_adapter',
-		//'admin_whitelist'
-	);
+	protected $_filters = array('login_adapter','admin_whitelist');
 
 	/**
 	 * @var array  Options that are used in the ldap plugin.
 	 */
 	protected $_options = array(
-		'ldap_server' => 'localhost',
+		'ldap_host' => 'localhost',
 		'ldap_port' => 389,
-		'ldap_basedn' => "",
+		'ldap_baseDn' => "",
 		'ldap_accountCanonicalForm' => 1,
 		'ldap_accountFilterFormat' => "",
 		'ldap_accountDomainName' => "",
@@ -70,19 +67,47 @@ class LdapPlugin extends Omeka_Plugin_AbstractPlugin
 	}
 
 	/**
-	 * Helps filter out the login.
+	 * Bypasses the normal login functionality and checks the LDAP server
+	 * to authenticate the user.
+	 *
+	 * @param  Omeka_Auth_Adapter_UserTable  $adapter   The auth adapter for users
+	 * @param  array                         $form      The login form
+	 * @return LdapAuthAdapter
 	 */
-	public function filterLoginAdapter()
+	public function filterLoginAdapter($adapter, $form)
 	{
+		$user = $form['login_form']->getValue('username');
+		$password = $form['login_form']->getValue('password');
 
+		$ldap = array();
+		foreach (array_keys($this->_options) as $key)
+		{
+			$name = substr($key, 5);
+			$ldap[$name] = get_option($key);
+		}
+
+		/**
+		 * THe auth adapter should be autoloaded, but I'm not that familiar with Zend...
+		 */
+		require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'LdapAuthAdapter.php';
+		$ldap_adapter = new LdapAuthAdapter(array('ldap' => $ldap), $user, $password);
+		return $ldap_adapter;
 	}
 
 	/**
-	 * Administration Whitelist.
+	 * Adds some pages to the administration whitelist
+	 *
+	 * @param  array $list  The current whitelist
+	 * @return array        An updated whitelist
 	 */
-	public function filterAdminWhitelist()
+	public function filterAdminWhitelist($list)
 	{
+		$list[] = array(
+			'controller' => 'ldap',
+			'action' => 'forgot-password'
+		);
 
+		return $list;
 	}
 
 }
